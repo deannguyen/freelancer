01fce1ecce9334aedbd4327d5b191d0b20f1dd4e
Nguyen Dat <nguyendata7@yahoo.com>
2014-05-16 11:39:07 +0700

    First time register Freelancer Plugins

---
+++ freelancer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: Freelancer Plugin
+  Plugin URI: http://webtretho.com/
+  Description: This plugin's build for Freelancer box
+  Version: 1.0.0
+  Author: Nguyen Dat
+  Author URI: http://webtretho.com/
+ */
